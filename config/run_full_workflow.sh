2c06ebc1262d7154b8da0f0b0159f0b57d86314a
apsteinberg <asherps@gmail.com>
2024-11-10 18:47:49 -0500

    sv_merge rule

---
+++ config/run_full_workflow.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#SBATCH --partition=componc_cpu
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=6:00:00
+#SBATCH --mem=8GB
+#SBATCH --job-name=10.1
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=preskaa@mskcc.org
+#SBATCH --output=slurm%j_snkmk.out
+
+### example slurm submission script ###
+
+## set directories
+tag=APS017.1
+pipeline_dir=$HOME/SVAnnotator
+#logdir=${submitdir}/log
+outdir=/data1/shahs3/users/preskaa/${tag}_Archive/SVAnnotator_test
+config_yaml=config/config.yml
+profile_yaml=${pipeline_dir}/workflow/profiles/
+snakefile=${pipeline_dir}/workflow/Snakefile
+## switch to the right conda environment
+source /home/preskaa/miniconda3/bin/activate snakemake
+
+mkdir -p ${outdir}
+
+### run the pipeline
+echo "Current working directory: $(pwd)"
+echo "Snakefile path: ${snakefile}"
+
+# this will run the process, for example you could switch the below to a python command:
+# python script.py
+cd ${pipeline_dir}
+
+snakemake \
+  --snakefile ${snakefile} \
+  --profile ${profile_yaml} \
+  --configfile ${config_yaml}\
+  --conda-prefix /data1/shahs3/users/preskaa/conda \
+  --singularity-prefix /data1/shahs3/users/preskaa/singularity \
+  --singularity-args "--bind /data1/shahs3" \
+  --dry-run
--- workflow/rules/sv_merge.smk
+++ workflow/rules/sv_merge.smk
@@ -1,2 +1,29 @@
+import os
+out_dir = config["out_dir"]
 # merge SVs
 rule sv_merge:
+    input:
+        tsv=config["samples"]
+    output:
+        merged_vcf=os.path.join(out_dir, "minda", config["sample_name"]+"_minda_ensemble.vcf")
+    params:
+        sample_name=config["sample_name"],
+        out_dir=os.path.join(out_dir, "minda"),
+        min_support=2,
+        tolerance=100,
+        min_size=50
+    resources:
+        mem_mb = 20000,
+        time = 360,
+        retries = 0
+    threads: 1,
+    container:
+        "docker://quay.io/preskaa/minda:v241109",
+    shell:
+        """
+        ./minda.py ensemble --tsv {input.tsv} --outdir {params.out_dir} \
+        --min_support {params.min_support} --tolerance {params.tolerance} \
+        --min_size {params.min_size}
+        """
+
+
